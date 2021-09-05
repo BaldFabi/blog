fbd1ea36752959f366dbe6416f5b8619e3159806
BaldFabi <thebaldfabi@gmail.com>
2021-09-05 12:23:28 +0200

    Fix purgecss config

--- themes/baldfabi/assets/css/postcss.config.js
+++ themes/baldfabi/assets/css/postcss.config.js
@@ -2,11 +2,7 @@ const themeDir = __dirname + "/../../";
 const { join } = require("path");
 
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: [
-    join(process.cwd(), "hugo_stats.json"),
-    join(process.cwd(), "layouts/**/*.html"),
-    join(__dirname, "layouts/**/*.html")
-  ],
+  content: [join(themeDir, "layouts/**/*.html")],
   extractors: [
     {
       extractor: (content) => JSON.parse(content).htmlElements.classes,
